2ee29c00392a422be4df5255dc823208507f5cb6
dbtlearning2023@gmail.com <dbtlearning2023@gmail.com>
2023-01-18 07:08:15 +0000

    commit18

---
+++ snapshots/snapshot_timestamp.sql
@@ -0,0 +1,16 @@
+{% snapshot snapshot_timestamp %}
+
+{{
+
+ config(
+target_database='PROD',    
+ target_schema='snap',
+ unique_key='id',
+ strategy='timestamp',
+ updated_at = '_ETL_LOADED_AT'
+       )
+
+}}
+select * FROM {{ source('jaffle_shop', 'orders') }}
+
+{% endsnapshot %}
